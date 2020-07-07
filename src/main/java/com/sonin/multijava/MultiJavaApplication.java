86a80c4da6e4d4427608cf41d7fde17a2e7eb9ef
songning123456 <772805406sn123>
2020-07-07 23:25:06 +0800

    first commit

---
+++ src/main/java/com/sonin/multijava/MultiJavaApplication.java
@@ -0,0 +1,13 @@
+package com.sonin.multijava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sonin/multijava/MultiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonin.multijava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
