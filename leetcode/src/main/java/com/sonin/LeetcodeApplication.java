08d0cd9c73bf639dc1eb2bdeb721157f31f05db1
songning123456 <1457065857@qq.com>
2020-07-10 21:22:37 +0800

    补充Application

---
+++ leetcode/src/main/java/com/sonin/LeetcodeApplication.java
@@ -0,0 +1,16 @@
+package com.sonin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author sonin
+ * @date 2020/7/10 21:19
+ */
+@SpringBootApplication
+public class LeetcodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeetcodeApplication.class, args);
+    }
+}
