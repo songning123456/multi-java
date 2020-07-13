1522ce1af13ae0d77b85f45c9e3afb8dedb0161b
songning123456 <1457065857@qq.com>
2020-07-13 16:48:24 +0800

    leetcode添加

---
+++ leetcode/src/main/java/com/sonin/arithmetic/MaxSubArray.java
@@ -0,0 +1,26 @@
+package com.sonin.arithmetic;
+
+/**
+ * @author sonin
+ * @date 2020/7/13 13:32
+ */
+public class MaxSubArray {
+
+    private static int maxSubArray1(int[] nums) {
+        int[] dp = new int[nums.length];
+        int max = nums[0];
+        dp[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+            if (max < dp[i]) {
+                max = dp[i];
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maxSubArray1(nums));
+    }
+}
