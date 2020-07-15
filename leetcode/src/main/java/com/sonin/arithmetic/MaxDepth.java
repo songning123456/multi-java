d77c129746a9f5221180a013310b4b6037f28be7
songning123456 <1457065857@qq.com>
2020-07-16 00:32:16 +0800

    leetcode添加

---
+++ leetcode/src/main/java/com/sonin/arithmetic/MaxDepth.java
@@ -0,0 +1,25 @@
+package com.sonin.arithmetic;
+
+/**
+ * @author sonin
+ * @date 2020/7/15 18:41
+ */
+public class MaxDepth {
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
