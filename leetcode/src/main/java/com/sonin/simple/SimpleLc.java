0c1d6dafbca9975aea0451bb3a79f7ee5aaec148
songning123456 <1457065857@qq.com>
2020-08-08 21:04:45 +0800

    leetcode

--- leetcode/src/main/java/com/sonin/simple/SimpleLc.java
+++ leetcode/src/main/java/com/sonin/simple/SimpleLc.java
@@ -1,6 +1,8 @@
 package com.sonin.simple;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author sonin
@@ -59,5 +61,114 @@ public class SimpleLc {
             res = Math.max(Math.abs(heaters[j] - houses[i]), res);
         }
         return res;
+    }
+
+    /**
+     * 数字的补数
+     *
+     * @param num
+     * @return
+     */
+    public static int findComplement(int num) {
+        StringBuilder stringBuilder = new StringBuilder();
+        while (num != 0) {
+            stringBuilder.append(num % 2);
+            num /= 2;
+        }
+        String str = stringBuilder.reverse().toString();
+        int sum = 0;
+        for (int i = str.length() - 1; i >= 0; i--) {
+            int k = str.charAt(i) - 48;
+            if (k == 0) {
+                int res = 1;
+                int m = str.length() - 1;
+                while (m > i) {
+                    res *= 2;
+                    m--;
+                }
+                sum = sum + res;
+            }
+        }
+        return sum;
+    }
+
+    /**
+     * 密钥格式化
+     *
+     * @param S
+     * @param K
+     * @return
+     */
+    public static String licenseKeyFormatting(String S, int K) {
+        S = S.toUpperCase();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < S.length(); i++) {
+            char cur = S.charAt(i);
+            if (i == 0 || i % K != 0) {
+                if (cur != '-') {
+                    stringBuilder.append(cur).append("-");
+                }
+            } else {
+                if (cur != '-') {
+                    stringBuilder.append(cur);
+                }
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    /**
+     * 最大连续1的个数
+     *
+     * @param nums
+     * @return
+     */
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0;
+        int ans = 0;
+        for (int num : nums) {
+            if (num == 1) {
+                ans++;
+            } else if (num == 0) {
+                max = Math.max(max, ans);
+                ans = 0;
+            }
+        }
+        return Math.max(max, ans);
+    }
+
+    /**
+     * 构造矩形
+     *
+     * @param area
+     * @return
+     */
+    public static int[] constructRectangle(int area) {
+        int[] min = new int[]{1, 1};
+        Map<Integer, Integer> map = new HashMap<>(2);
+        for (int l = 1; l <= area; l++) {
+            int w = area / l;
+            if (w * l == area && l >= w) {
+                map.put(l, w);
+            }
+        }
+        for (Map.Entry<Integer, Integer> item : map.entrySet()) {
+            int l = item.getKey();
+            int w = item.getValue();
+            if (min[0] == 1 && min[1] == 1) {
+                min[0] = l;
+                min[1] = w;
+            } else if (min[0] - min[1] > l - w) {
+                min[0] = l;
+                min[1] = w;
+            }
+        }
+        return min;
+    }
+
+    public static void main(String[] args) {
+//        findComplement(2);
+//        licenseKeyFormatting("2-5g-3-J", 2);
+        constructRectangle(2);
     }
 }
