09fde7d5fe0ba9e81bbe842ad27d84e57371e0c9
songning123456 <1457065857@qq.com>
2020-08-17 22:54:52 +0800

    leetcode

--- leetcode/src/main/java/com/sonin/simple/SimpleLc.java
+++ leetcode/src/main/java/com/sonin/simple/SimpleLc.java
@@ -404,6 +404,20 @@ public class SimpleLc {
         return (end - start > 0) ? (end - start + 1) : 0;
     }
 
+    public int findLHS(int[] nums) { int res = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        for (int key : map.keySet()) {
+            if (map.containsKey(key + 1)) {
+                res = Math.max(res, map.get(key) + map.get(key + 1));
+            }
+        }
+        return res;
+
+    }
+
     public static void main(String[] args) {
         SimpleLc simpleLc = new SimpleLc();
         int[] nums = new int[]{1, 2, 3, 5, 4};
