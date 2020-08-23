3bf59cebb0f7c096cf87a174157952f6bae3ca63
songning123456 <1457065857@qq.com>
2020-08-23 16:56:20 +0800

    leetcode

--- leetcode/src/main/java/com/sonin/labuladong/DynamicPlan.java
+++ leetcode/src/main/java/com/sonin/labuladong/DynamicPlan.java
@@ -93,6 +93,40 @@ public class DynamicPlan {
         return -1;
     }
 
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                dp[i][0] = 0;
+                dp[i][1] = -prices[i];
+            } else {
+                dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+                dp[i][1] = Math.max(dp[i - 1][1], -prices[i]);
+            }
+        }
+        return dp[n - 1][0];
+    }
+
+    public int maxProfit2(int[] prices) {
+        int n = prices.length;
+        if (n == 0) {
+            return 0;
+        }
+        int[][] dp = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                dp[i][0] = 0;
+                dp[i][1] = -prices[i];
+                continue;
+            }
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+        return dp[n - 1][0];
+    }
+
+
     public int[] searchRange(int[] nums, int target) {
         int[] res = new int[2];
         int left = 0, right = nums.length - 1;
