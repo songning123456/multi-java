f0393e14feaadafc378b31a0651bb885db64164a
sonin <1457065857@qq.com>
2020-11-24 10:56:20 +0800

    添加netty

---
+++ bugstack-netty/src/main/java/com/bugstack/netty/NettyApplication.java
@@ -0,0 +1,16 @@
+package com.bugstack.netty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author sonin
+ */
+@SpringBootApplication
+public class NettyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyApplication.class, args);
+    }
+
+}
---
+++ bugstack-netty/src/main/java/com/bugstack/netty/server/MyChannelInitializer.java
@@ -0,0 +1,8 @@
+package com.bugstack.netty.server;
+
+/**
+ * @author sonin
+ * @date 2020/11/24 10:53
+ */
+public class MyChannelInitializer {
+}
