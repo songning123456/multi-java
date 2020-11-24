8d839a8bcf9746b06bcb21588320bc6ddedcf28d
sonin <1457065857@qq.com>
2020-11-24 09:10:12 +0800

    优化

---
+++ base/src/main/java/com/sonin/base/test/ConfigExample.java
@@ -0,0 +1,55 @@
+package com.sonin.base.test;
+
+import com.alibaba.nacos.api.NacosFactory;
+import com.alibaba.nacos.api.PropertyKeyConst;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.config.listener.Listener;
+import com.alibaba.nacos.api.exception.NacosException;
+
+import java.util.Properties;
+import java.util.concurrent.Executor;
+
+/**
+ * @author sonin
+ * @date 2020/11/13 15:37
+ */
+public class ConfigExample {
+
+    public static void main(String[] args) throws NacosException, InterruptedException {
+        String serverAddr = "192.168.8.113";
+        String dataId = "blade-dev.yaml";
+        String group = "DEFAULT_GROUP";
+        Properties properties = new Properties();
+        properties.put(PropertyKeyConst.SERVER_ADDR, serverAddr);
+        ConfigService configService = NacosFactory.createConfigService(properties);
+        String content = configService.getConfig(dataId, group, 5000);
+        System.out.println(content);
+        configService.addListener(dataId, group, new Listener() {
+            @Override
+            public void receiveConfigInfo(String configInfo) {
+                System.out.println("receive:" + configInfo);
+            }
+
+            @Override
+            public Executor getExecutor() {
+                return null;
+            }
+        });
+
+//        boolean isPublishOk = configService.publishConfig(dataId, group, "content");
+//        System.out.println(isPublishOk);
+//
+//        Thread.sleep(3000);
+//        content = configService.getConfig(dataId, group, 5000);
+//        System.out.println(content);
+//
+//        boolean isRemoveOk = configService.removeConfig(dataId, group);
+//        System.out.println(isRemoveOk);
+//        Thread.sleep(3000);
+//
+//        content = configService.getConfig(dataId, group, 5000);
+//        System.out.println(content);
+//        Thread.sleep(300000);
+
+    }
+}
