fb4c2aa771ca08cb2c4536ffe47e9bcb0ebbd092
sonin <1457065857@qq.com>
2020-12-02 18:55:02 +0800

    添加netty

---
+++ base/src/main/java/com/sonin/base/roundrobin/Server.java
@@ -0,0 +1,10 @@
+package com.sonin.base.roundrobin;
+
+/**
+ * @author sonin
+ * @date 2020/11/25 16:16
+ */
+public class Server {
+    private String ip;
+    private int weight;
+}
--- bugstack-netty/src/main/java/com/bugstack/netty/server/MyClientHandler.java
+++
@@ -1,8 +0,0 @@
-package com.bugstack.netty.server;
-
-/**
- * @author sonin
- * @date 2020/11/24 10:53
- */
-public class MyClientHandler {
-}
