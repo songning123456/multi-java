ec1e603af39dbad81844d2823e18acce20e1a704
sonin <1457065857@qq.com>
2020-12-25 11:04:12 +0800

    second view

---
+++ leetcode/src/main/java/com/sonin/entity/ListNode.java
@@ -0,0 +1,14 @@
+package com.sonin.entity;
+
+/**
+ * @author sonin
+ * @date 2020/12/22 9:35
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int x) {
+        val = x;
+    }
+}
